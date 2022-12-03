9f6a306dbbeb53995ce15008099bc5dadc5f6ad4
NullUsxr <100100163+NullUsxr@users.noreply.github.com>
2022-12-03 11:39:58 +0000

    Optimized string parsing + fancier text

--- AutoPiano.py
+++ AutoPiano.py
@@ -1,44 +1,45 @@
-# Auto-Piano
 # NullUsxr 2022
 import pyautogui as gui
-import time as tm
-
-
+import time
+class Clr:
+    fg = '\033[7m'
+    success = '\033[92m'
+    stat = '\033[96m'
+    info = '\033[93m'
+    error = '\033[91m'
+    endc = '\033[0m'
 def play(song):
-    print("[ALERT] Now playing song")
-    tm.sleep(5)
-    counter = 0
-    clockspeed = gui.PAUSE
-    while counter < len(song):
-        for i in range(10):  # Parse most unneeded text
-            if counter < len(song):  # Prevent string index out of range crash
-                if song[counter] == "[":
-                    gui.PAUSE = 0
-                    counter = counter + 1
-            if counter < len(song):
-                if song[counter] == " ":
-                    tm.sleep(pause)
-                    counter = counter + 1
-                    print("[INFO] Paused")
-            if counter < len(song):
-                if song[counter] == "|":
-                    tm.sleep(longpause)
-                    counter = counter + 1
-                    print("[INFO] Long Paused")
-            if counter < len(song):
-                if song[counter] == "]":
-                    gui.PAUSE = clockspeed
-                    counter = counter + 1
-        if counter < len(song):
-            gui.press(song[counter])
-            counter = counter + 1
-            print("[INFO] Played", counter, "/", len(song))
-
-
+    print(f"{Clr.stat}[ALERT]{Clr.endc} Playing song!")
+    time.sleep(5)
+    i = 0
+    while i < len(song):
+        if song[i] == "[":
+            i += 1
+            gui.PAUSE = 0
+        elif song[i] == "]":
+            i += 1
+            gui.PAUSE = delay
+        elif song[i] == " ":
+            time.sleep(pause)
+            print(f"{Clr.info}[INFO]{Clr.endc} Paused")
+            i += 1
+        elif song[i] == "|":
+            time.sleep(longpause)
+            print(f"{Clr.info}[INFO]{Clr.endc} Long Paused")
+            i += 1
+        else:
+            gui.typewrite(song[i])
+            print(f"{Clr.info}[INFO]{Clr.endc} Played note", "{}/{}".format(i, len(song)))
+            i += 1
 while 1 == 1:
-    print("NullUsxr's AutoPiano Program")
-    pause = float(input("Pause: "))
-    longpause = float(input("Long Pause: "))
-    gui.PAUSE = float(input("Playback Speed: "))
-    play(input("Song: "))
-    print("[ALERT] Finished playing song")
+    print(f"{Clr.fg}NullUsxr's AutoPiano{Clr.endc}")
+    try:
+        pause = float(input("Pause> "))
+        longpause = float(input("Long Pause> "))
+        delay = float(input("Delay> "))
+        gui.PAUSE = delay
+        play(input("Song> "))
+        print(f"{Clr.success}[SUCCESS]{Clr.endc} Finished playing song!")
+    except ValueError:
+        print(f"{Clr.error}[ERROR]{Clr.endc} Invalid number!")
+        pass
